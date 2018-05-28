a13728301be95438e1c41e3e8aa9993808ab2539
KUBASUS2003 <1kuba03@gmail.com>
2018-05-28 16:13:14 +0200

    aa

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Zmienne PRIVATE
+        private short test;
+        #endregion
+        #region INTERNAL
+
+        #endregion
+
+        #region Konstruktor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+
+        #region Metods
+        /// <summary>
+        /// a test method do not use
+        /// </summary>
+        /// <param name="a"></param> 
+        public void metod1(int a)
+        {
+
+        }
+        #endregion
+
     }
 }
